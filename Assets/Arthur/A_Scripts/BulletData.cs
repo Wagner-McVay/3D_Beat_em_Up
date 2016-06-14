af8622ec78fd51997e22030ed78924709013c2be
GilyeatArthur <gilyeatarthur@hotmail.com>
2016-06-14 11:34:18 -0700

    Fixed bullets and made them Shiny

---
+++ Assets/Arthur/A_Scripts/BulletData.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletData : MonoBehaviour {
+
+    int Damage;
+    float Decay;
+    float LastDecay;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Decay = GameObject.FindGameObjectWithTag("Player").GetComponent<Bullet>().BulletDecay;
+        LastDecay = Decay + Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > LastDecay)
+        { Destroy(); }
+	}
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Terrain")
+        {
+            Destroy();
+        }
+    }
+
+    void Destroy()
+    { Destroy(this.gameObject); }
+}
